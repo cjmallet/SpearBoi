33531a2850a6f002918131c37b34f320fa724b40
Casper Arends <casper.arends@ing.com>
2022-05-02 14:55:28 +0200

    Level layout + ring working with doors + test level

--- SpearBoi/Assets/Scripts/Ring.cs
+++ SpearBoi/Assets/Scripts/Ring.cs
@@ -7,10 +7,15 @@ public class Ring : MonoBehaviour
     // Start is called before the first frame update
     public bool ringActive;
     [SerializeField] private Material myMaterial;
+    SpriteRenderer switchSprite;
+    [SerializeField]
+    private GameObject door;
+
 
     public void Start()
     {
         ringActive = false;
+        Deactivate();
     }
 
     public void Update()
@@ -34,7 +39,27 @@ public class Ring : MonoBehaviour
         Debug.Log("oets");
         if (collision.transform.CompareTag("Spear"))
         {
-            ringActive = true;
+            switchState();
+        }
+    }
+
+    public void switchState() //Wordt aangeroepen wanneer de chain collision heeft met de switch
+    {
+        
+        if (ringActive == false)
+        {
+            Activate();
+            door.GetComponent<SpriteRenderer>().sprite = door.GetComponent<DoorScript>().doorOpen.GetComponent<SpriteRenderer>().sprite;
+            door.GetComponent<BoxCollider2D>().enabled = false;
+            ringActive = !ringActive;
+        }
+        else if (ringActive == true)
+        {
+            Deactivate();
+            door.GetComponent<SpriteRenderer>().sprite = door.GetComponent<DoorScript>().doorClosed.GetComponent<SpriteRenderer>().sprite;
+            door.GetComponent<BoxCollider2D>().enabled = true;
+            ringActive = !ringActive;
+
         }
     }
 }
