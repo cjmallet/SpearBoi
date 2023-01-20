402ceb0c88e741df8cda00643aa57520be2e8cc8
Aslesh <cjullienmallet@gmail.com>
2023-01-20 21:25:46 +0100

    add particle system to weakpoint and indicator of current attack

--- SpearBoi/Assets/Scripts/BossManager.cs
+++ SpearBoi/Assets/Scripts/BossManager.cs
@@ -18,6 +18,7 @@ public class BossManager : MonoBehaviour
     private GameObject leftArmor, rightArmor, leftArm, rightArm;
     private SawSpitter leftSpitter, rightSpitter;
     private float timer;
+    private SpriteRenderer eyeRenderer;
     private bool canAttack, leftDestroyed,rightDestroyed, isDying;
 
     private void Awake()
@@ -32,6 +33,7 @@ public class BossManager : MonoBehaviour
         leftSpitter = leftArm.transform.GetChild(0).GetComponent<SawSpitter>();
         rightSpitter = rightArm.transform.GetChild(0).GetComponent<SawSpitter>();
 
+        eyeRenderer = transform.GetChild(5).GetComponent<SpriteRenderer>();
         ResetToBase();
     }
 
@@ -85,10 +87,16 @@ public class BossManager : MonoBehaviour
         rightSpitter.shotgunMode = false;
         timer = 0;
         canAttack = true;
+        eyeRenderer.color = Color.white;
+        leftSpitter.GetComponent<SpriteRenderer>().color = Color.white;
+        rightSpitter.GetComponent<SpriteRenderer>().color = Color.white;
     }
 
     private IEnumerator SprayAttack()
     {
+        eyeRenderer.color = Color.red;
+        leftSpitter.GetComponent<SpriteRenderer>().color = Color.red;
+        rightSpitter.GetComponent<SpriteRenderer>().color = Color.red;
         canAttack = false;
         leftSpitter.shootTimer = 0.3f;
         rightSpitter.shootTimer = 0.3f;
@@ -113,6 +121,9 @@ public class BossManager : MonoBehaviour
 
     private IEnumerator HomingAttack()
     {
+        eyeRenderer.color = Color.green;
+        leftSpitter.GetComponent<SpriteRenderer>().color = Color.green;
+        rightSpitter.GetComponent<SpriteRenderer>().color = Color.green;
         canAttack = false;
         RotateArms(90);
         leftSpitter.shootTimer = 20f;
@@ -131,6 +142,9 @@ public class BossManager : MonoBehaviour
 
     private IEnumerator ShotgunBlast()
     {
+        eyeRenderer.color = Color.yellow;
+        leftSpitter.GetComponent<SpriteRenderer>().color = Color.yellow;
+        rightSpitter.GetComponent<SpriteRenderer>().color = Color.yellow;
         canAttack = false;
         RotateArms(50);
         leftSpitter.shootTimer = 20f;
@@ -146,6 +160,8 @@ public class BossManager : MonoBehaviour
 
     public void TakeDamage(Direction side)
     {
+        
+
         switch (side)
         {
             case Direction.LEFT:
@@ -177,14 +193,23 @@ public class BossManager : MonoBehaviour
                 }
                 break;
         }
+
+        if (health==2)
+        {
+            transform.Find("Weakness").GetComponent<ParticleSystem>().Play();
+        }
     }
 
     private IEnumerator GetDestroyed()
     {
+        eyeRenderer.color = Color.black;
+        leftSpitter.GetComponent<SpriteRenderer>().color = Color.black;
+        rightSpitter.GetComponent<SpriteRenderer>().color = Color.black;
         isDying = true;
         Instantiate(bossCoinExplosion, transform);
-        leftSpitter.shootTimer = 20;
-        rightSpitter.shootTimer = 20;
+        leftSpitter.shootTimer =100000;
+        rightSpitter.shootTimer = 100000;
+        GetComponent<Animator>().SetBool("Dying",true);
         Destroy(doorBlockade);
         yield return new WaitForSeconds(4);
         Destroy(this.gameObject);
